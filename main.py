db34d876428f332724032b78b012bd7a7ad44f81
Illia <illiakucherenko95@gmail.com>
2022-02-09 23:49:28 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+s = float(input('Input s > '))
+t = float(input('Input t > '))
+V = s / t
+
+print(f'V = {V}')
+
+
+v = float(input('Input v > '))
+v0 = float(input('Input v0 > '))
+t = float(input('Input t > '))
+a = (v - v0) / t
+
+print(f'a = {a}')
+
+
+v0 = float(input('Input v0 > '))
+t = float(input('Input t > '))
+a = float(input('Input a > '))
+s = v0 * t + (a * t ** 2)/2
+
+print(f's = {s}')
+
+
+f = float(input('Input F > '))
+m = float(input('Input m > '))
+a = f / m
+
+print(f'a = {a}')
+
+
+m1 = float(input('Input m1 > '))
+m2 = float(input('Input m2 > '))
+r = float(input('Input R > '))
+g = float(input('Input G > '))
+f = g * ((m1 * m2) / r ** 2)
+
+print(f'F = {f}')
+
+
+v = float(input('Input v > '))
+r = float(input('Input R > '))
+a = v ** 2 / r
+
+print(f'a = {a}')
+
+
+m = float(input('Input m > '))
+v = float(input('Input v > '))
+p = m * v
+
+print(f'p = {p}')
+
+
+v = float(input('Input v > '))
+t = 1 / v
+
+print(f'T = {t}')
+
+
+v = float(input('Input v > '))
+t = float(input('Input t > '))
+l = v * t
+
+print(f'l = {l}')
